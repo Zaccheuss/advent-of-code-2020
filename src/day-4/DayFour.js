98e06a5318471e25f5bf3d7b6c8207a832a71983
Zachary <dragonslayerzac@gmail.com>
2020-12-12 21:26:57 -0500

    Complete day four second star

--- src/day-4/DayFour.js
+++ src/day-4/DayFour.js
@@ -32,15 +32,33 @@ function convertTextToPassport(input) {
     return Object.fromEntries(splitArray);
 }
 
-function checkValidity(passport) {
+function checkValidity(pp) {
     // abstact equality operator will match undefined and null in this case
-    if (passport.byr == null ||
-        passport.iyr == null ||
-        passport.eyr == null ||
-        passport.hgt == null ||
-        passport.hcl == null ||
-        passport.ecl == null ||
-        passport.pid == null) { return false; }
+    if (pp.byr == null ||
+        pp.iyr == null ||
+        pp.eyr == null ||
+        pp.hgt == null ||
+        pp.hcl == null ||
+        pp.ecl == null ||
+        pp.pid == null) { return false; }
+
+    if (!(pp.byr >= 1920 && pp.byr <= 2002)) { return false; }
+    if (!(pp.iyr >= 2010 && pp.iyr <= 2020)) { return false; }
+    if (!(pp.eyr >= 2020 && pp.eyr <= 2030)) { return false; }
+    
+    let unitlessHeight = pp.hgt.match(/\d+/)[0];
+    if (pp.hgt.includes('cm')) { 
+        if (!(unitlessHeight >= 150 && unitlessHeight <= 193)) { return false; }
+    } else { //height is in inches
+        if (!(unitlessHeight >= 59 && unitlessHeight <= 76)) { return false; }
+    }
+
+    if (pp.hcl.match(/^#[0-9a-f]+$/) === null) { return false; }
+    
+    let validEyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    if (!validEyeColors.includes(pp.ecl)) { return false; }
+    if (!(pp.pid.toString().length === 9)) { return false; }
+
     return true;
 }
 
